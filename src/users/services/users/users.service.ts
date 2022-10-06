9b3419452a211ecd442039f573b6d22a38ed46ed
WallaceGomes <wallacecardosogomes@gmail.com>
2022-10-05 23:33:01 -0300

    chore: add users module

---
+++ src/users/services/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  indexUsers() {
+    return 'indexUsers';
+  }
+
+  createUser() {
+    return 'createUsers';
+  }
+}
