36247720382af6a9aa5fe59cac9fc641a1ff0450
WallaceGomes <wallacecardosogomes@gmail.com>
2022-10-06 23:17:22 -0300

    chore: add user CRUD

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -3,9 +3,10 @@ import {
   Get,
   Post,
   Put,
-  Patch,
   Delete,
   Body,
+  Param,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 import { UsersService } from 'src/users/services/users/users.service';
@@ -13,10 +14,26 @@ import { UsersService } from 'src/users/services/users/users.service';
 export class UsersController {
   constructor(private userService: UsersService) {}
   @Get()
-  getUsers() {}
+  async getUsers() {
+    const users = await this.userService.indexUsers();
+    return users;
+  }
 
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
-    this.userService.createUser();
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: CreateUserDto,
+  ) {
+    return this.userService.updateUser({ ...updateUserDto, id });
+  }
+
+  @Delete()
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.deleteUser(id);
   }
 }
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/User';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -10,11 +12,37 @@ export class UsersService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  indexUsers() {
-    return 'indexUsers';
+  public async indexUsers() {
+    return this.userRepository.find();
   }
 
-  createUser() {
-    return 'createUsers';
+  public async createUser(data: CreateUserDto) {
+    const createdUser = this.userRepository.create({
+      ...data,
+      createdAt: new Date(),
+    });
+
+    return this.userRepository.save(createdUser);
+  }
+
+  public async updateUser(data: UpdateUserDto) {
+    const updatedUser = await this.userRepository.update(
+      { id: data.id },
+      { ...data },
+    );
+
+    return updatedUser;
+  }
+
+  public async deleteUser(id: number) {
+    const userToDelte = await this.userRepository.findOne({
+      where: { id },
+    });
+
+    if (!userToDelte) {
+      throw new Error('User not found');
+    }
+
+    await this.userRepository.remove(userToDelte);
   }
 }
